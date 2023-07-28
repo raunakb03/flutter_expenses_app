353c01a981e86d0aedeca3a65b45edd651598dd8
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-07-28 23:33:06 +0530

    creating project and creating class for expenses and creating initial expenses

---
+++ lib/expenses.dart
@@ -0,0 +1,39 @@
+import 'package:exepense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.99,
+      date: DateTime.now(),
+      category: Category.leasure,
+    ),
+  ];
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: const [
+          Text("this is the top bar"),
+          Text('This is the lower section'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:exepense_tracker/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
