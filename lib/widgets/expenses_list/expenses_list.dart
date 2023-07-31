2e92aa2b90fe38764012ab24f6354a4aaf6fbd34
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-07-31 23:23:17 +0530

    adding custom styling with theming

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,6 +1,7 @@
 import 'package:exepense_tracker/models/expense.dart';
 import 'package:exepense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
@@ -18,6 +19,12 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (ctx, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.75),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
         onDismissed: (directon) {
           onRemoveExpense(expenses[index]);
         },
